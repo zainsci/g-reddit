047a821e1eed73d895aee48942d76e09a19f997f
zainsci <zainansari08@gmail.com>
2020-12-25 21:57:18 +0500

    Navbar Styling Done

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,13 +1,30 @@
 const NavBar = () => {
   return (
     <>
-      <header>
-        <nav>
-          <div className="logo">GReddit</div>
+      <header className="w-full border-b">
+        <nav className="flex justify-between items-center py-2 px-6">
+          <a href="/">
+            <div className="text-2xl">
+              <span className="font-semibold">G</span>Reddit
+            </div>
+          </a>
           <form>
-            <input type="text" className="search" name="search" />
+            <input
+              type="text"
+              className="w-96 p-3 border rounded-lg bg-gray-100 focus:outline-none focus:shadow-md"
+              autoComplete="off"
+              name="search"
+              placeholder="Search Mail"
+            />
           </form>
-          <div className="settings"></div>
+          <div className="flex">
+            <a href="/">
+              <div className="border rounded-full w-10 h-10 mx-2 bg-green-500"></div>
+            </a>
+            <a href="/">
+              <div className="border rounded-full w-10 h-10 mx-2 bg-red-600"></div>
+            </a>
+          </div>
         </nav>
       </header>
     </>
