4bf56482de1c29c43bab8595ba3bde2d51f41755
zainsci <zainansari08@gmail.com>
2020-12-26 20:20:23 +0500

    Add Styling to SideBar

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -2,15 +2,19 @@ import SideList from "./SideList";
 
 const SideBar = () => {
   return (
-    <div className="sidebar">
-      <div>
-        <div className="compose">Compose</div>
-      </div>
-      <div className="upper__list">
-        <SideList list={["Inbox", "Starred", "Sent", "Snoozed"]} />
-      </div>
-      <div className="lower__list">
-        <SideList list={["New Meeting", "Join A Meeting"]} />
+    <div className="w-80">
+      <div className="w-full">
+        <div className="p-4 mb-5">
+          <button className="px-5 py-2 rounded-full bg-white shadow-md border focus:outline-none hover:shadow-lg">
+            <div className="font-medium text-lg">Compose</div>
+          </button>
+        </div>
+        <div className="upper__list">
+          <SideList list={["Inbox", "Starred", "Sent", "Snoozed"]} />
+        </div>
+        <div className="lower__list">
+          <SideList list={["New Meeting", "Join A Meeting"]} />
+        </div>
       </div>
     </div>
   );
--- src/Components/SideList.js
+++ src/Components/SideList.js
@@ -1,18 +1,42 @@
+import { useState } from "react";
+
 const SideList = (props) => {
   return (
     <ul className="list">
-      {props.list.map((listItem) => {
-        return (
-          <>
-            <li className="list__item">
-              <a href="/{listItem}" className="list__link">
-                {listItem}
-              </a>
-            </li>
-          </>
-        );
+      {props.list.map((item) => {
+        return <ListItem item={item} />;
       })}
     </ul>
+  );
+};
+
+const ListItem = (props) => {
+  const [active, setActive] = useState(false);
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    const allAnchores = document.querySelectorAll(".list-item");
+    allAnchores.forEach((item) => {
+      item.classList.remove("bg-red-200");
+      item.classList.remove("hover:bg-red-200");
+    });
+
+    setActive(true);
+  };
+  return (
+    <>
+      <li
+        className={`hover:bg-gray-100 rounded-r-full pointer list-item ${
+          active ? "bg-red-200 hover:bg-red-200" : ""
+        }`}
+      >
+        <a href={`/${props.item}`} className="" onClick={handleClick}>
+          <div className="py-2 px-5">
+            <div className="font-medium">{props.item}</div>
+          </div>
+        </a>
+      </li>
+    </>
   );
 };
 
