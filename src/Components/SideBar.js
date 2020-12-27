a5b6d414e8a1a53a38c971587f2b99f89b419d26
zainsci <zainansari08@gmail.com>
2020-12-27 13:20:08 +0500

    Side Bar Stlying

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -2,7 +2,7 @@ import SideList from "./SideList";
 
 const SideBar = () => {
   return (
-    <div className="w-80">
+    <div className="w-80 border-r">
       <div className="w-full">
         <div className="p-4 mb-5">
           <button className="px-5 py-2 rounded-full bg-white shadow-md border focus:outline-none hover:shadow-lg">
@@ -13,6 +13,7 @@ const SideBar = () => {
           <SideList list={["Inbox", "Starred", "Sent", "Snoozed"]} />
         </div>
         <div className="lower__list">
+          <div className="px-5 py-2 font-bold">Meeting</div>
           <SideList list={["New Meeting", "Join A Meeting"]} />
         </div>
       </div>
