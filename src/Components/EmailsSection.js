96873b98fa1b638d5775b32a941471a8ad9482f7
zainsci <zainansari08@gmail.com>
2020-12-27 15:01:21 +0500

    Add Fetch Request to EmialSection

--- src/Components/EmailsSection.js
+++ src/Components/EmailsSection.js
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react";
+import { Route } from "react-router-dom";
+
+import Email from "./Email";
 import Emails from "./Emails";
 
-const EmailsSection = () => {
+const EmailsSection = (props) => {
+  const [emails, setEmails] = useState([]);
+
+  useEffect(() => {
+    fetch("https://www.reddit.com/.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setEmails({ emails: data.data.children.slice(0, 10) });
+      });
+  }, []);
+
   return (
     <section>
-      <Emails />
+      {emails ? (
+        <>
+          <Route path="/" exact>
+            <div className="p-3 w-full border-b">
+              <div className="flex justify-between items-center">
+                <div></div>
+                <div className="">{emails.length}</div>
+              </div>
+            </div>
+            <Emails emails={emails} />
+          </Route>
+          <Route path="/r/">
+            <Email />
+          </Route>
+        </>
+      ) : (
+        <div></div>
+      )}
     </section>
   );
 };
