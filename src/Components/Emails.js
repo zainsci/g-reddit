53f3529b9f42c7d70a79161839148b0573bee30d
zainsci <zainansari08@gmail.com>
2020-12-27 15:01:47 +0500

    Add Mapping to Emails

--- src/Components/Emails.js
+++ src/Components/Emails.js
@@ -1,5 +1,24 @@
-const Emails = () => {
-  return <div>Emails</div>;
+import { Link } from "react-router-dom";
+
+const Emails = (props) => {
+  const emails = props.emails.emails;
+  console.log(emails);
+
+  return (
+    <div>
+      {emails
+        ? emails.map((email) => {
+            return (
+              <Link key={email.data.id} to={email.data.permalink}>
+                <div className="p-2 font-medium border-b hover:shadow-md cursor-pointer">
+                  <div>{email.data.title}</div>
+                </div>
+              </Link>
+            );
+          })
+        : null}
+    </div>
+  );
 };
 
 export default Emails;
