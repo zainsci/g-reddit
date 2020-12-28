e0c129365b79b29dce600f5a6db4f93e32b3b596
zainsci <zainansari08@gmail.com>
2020-12-28 12:03:45 +0500

    Add Button At Top BAr

--- src/Components/EmailsSection.js
+++ src/Components/EmailsSection.js
@@ -11,7 +11,7 @@ const EmailsSection = (props) => {
     fetch("https://www.reddit.com/.json")
       .then((res) => res.json())
       .then((data) => {
-        setEmails({ emails: data.data.children.slice(0, 10) });
+        setEmails({ emails: data.data.children });
       });
   }, []);
 
@@ -22,20 +22,63 @@ const EmailsSection = (props) => {
           className={"overflow-hidden"}
           style={{ height: window.innerHeight - 67 }}
         >
-          <div className="overflow-y-scroll h-full">
-            <Route path="/" exact>
-              <div className="p-3 w-full border-b">
-                <div className="flex justify-between items-center">
-                  <div></div>
-                  <div className="">{emails.length}</div>
+          <Route path="/" exact>
+            <div className="py-1 px-2 w-full border-b">
+              <div className="flex justify-between items-center">
+                <div className="flex">
+                  <div
+                    className="flex p-3 rounded-full cursor-pointer hover:bg-gray-200 hover:border"
+                    title="Select"
+                  >
+                    <input
+                      type="checkbox"
+                      name="select"
+                      id="Select"
+                      className="cursor-pointer w-4 h-4"
+                      title="Select"
+                    />
+                  </div>
+                  <div
+                    className="flex p-3 rounded-full cursor-pointer hover:bg-gray-200 hover:border"
+                    title="Refresh"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="fill-current w-4 h-4"
+                      viewBox="0 0 16 16"
+                      title="Refresh"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"
+                      />
+                      <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z" />
+                    </svg>
+                  </div>
+                  <div
+                    className="flex p-3 rounded-full cursor-pointer hover:bg-gray-200 hover:border"
+                    title="More"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="fill-current w-4 h-4"
+                      viewBox="0 0 16 16"
+                      title="More"
+                    >
+                      <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                    </svg>
+                  </div>
                 </div>
+                <div className="">{emails.length}</div>
               </div>
+            </div>
+            <div className="overflow-y-scroll h-full">
               <Emails emails={emails} />
-            </Route>
-            <Route path="/r/:subreddit/comments/:id/:perma/">
-              <Email />
-            </Route>
-          </div>
+            </div>
+          </Route>
+          <Route path="/r/:subreddit/comments/:id/:perma/">
+            <Email />
+          </Route>
         </div>
       ) : (
         <div></div>
