7f505972639ee5f5511429aa2e73954a2051c0fe
zainsci <zainansari08@gmail.com>
2020-12-28 13:38:27 +0500

    Add Time

--- src/Components/Emails.js
+++ src/Components/Emails.js
@@ -3,7 +3,17 @@ import StarOutline from "../SVGs/StarOutline";
 
 const Emails = (props) => {
   const emails = props.emails.emails;
-  console.log(emails);
+
+  const getTime = (timestamp) => {
+    console.log(timestamp);
+    const d = new Date(timestamp);
+    console.log(d.toLocaleString());
+    const date = d.getDate();
+    const month = d.getMonth();
+    const time = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+    console.log(time, date, month);
+    return `${time}, ${date}\\${month}`;
+  };
 
   return (
     <div>
@@ -11,14 +21,19 @@ const Emails = (props) => {
         ? emails.map((email) => {
             return (
               <Link key={email.data.id} to={email.data.permalink}>
-                <div className="overflow-hidden">
+                <div className="overflow-hidden text-sm hover:shadow-md">
                   <div className="py-1 px-2 flex items-center font-semibold border-b hover:shadow-md cursor-pointer flex">
-                    <div className="p-1.5 mr-2 rounded-full hover:border hover:bg-gray-200">
-                      <StarOutline />
+                    <div className="w-1/5 flex items-center overflow-hidden">
+                      <div className="p-1.5 mr-2 rounded-full hover:border hover:bg-gray-200">
+                        <StarOutline />
+                      </div>
+                      r/{email.data.subreddit}
                     </div>
-                    <div className="w-1/5 ">r/{email.data.subreddit}</div>
-                    <div className="w-2/5 whitespace-nowrap overflow-hidden">
+                    <div className="w-3/5 whitespace-nowrap overflow-hidden">
                       {email.data.title}
+                    </div>
+                    <div className="w-1/5 text-right pr-5">
+                      {getTime(email.data.created_utc)}
                     </div>
                   </div>
                 </div>
